74c4fad78ca6b4a95f1cd5a4df5d53a5dd4f172a
Abdullah Al Mahmud Joy <110623413+almahmudjoy@users.noreply.github.com>
2024-10-26 18:31:09 +0600

    login.php

--- admin/login.php
+++ admin/login.php
@@ -1,12 +1,10 @@
 <?php 
-
 	include '../components/connect.php';
 
 	$warning_msg = [];
 	$success_msg = [];
 
 	if (isset($_POST['login'])) {
-
 		$email = $_POST['email'];
 		$email = filter_var($email, FILTER_SANITIZE_STRING);
 
@@ -23,9 +21,7 @@
 		}else{
 			$warning_msg[] = 'incorrect email or password';
 		}
-
 	}
-
 ?>
 
 <!DOCTYPE html>
@@ -37,8 +33,15 @@
 	<title>DentiCare - dental clinic website template</title>
 
 	<!-- font awesome cdn link -->
-	<link rel="stylesheet" href="https://cdnjs.cloudeflare.com/ajax/libs/font-awesome/6.2.0.css/all.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0.css/all.min.css">
 	<link rel="stylesheet" type="text/css" href="../css/admin_style.css?v=<?php echo "time"; ?>">
+	<link rel="icon" href="../image/favicon.ico" type="image/x-icon">
+	<script>
+		function togglePasswordVisibility(id) {
+			const passwordField = document.getElementById(id);
+			passwordField.type = passwordField.type === "password" ? "text" : "password";
+		}
+	</script>
 </head>
 <body style="padding-left: 0;">
 <!-- register section starts -->
@@ -49,15 +52,16 @@
 
 		<div class="input-field">
 			<p>your email <span>*</span></p>
-				<input type="email" name="email" placeholder="enter your email" maxlength="50" required class="box">
-			
+			<input type="email" name="email" placeholder="enter your email" maxlength="50" required class="box">
 		</div>
 		<div class="input-field">
 			<p>your password <span>*</span></p>
-				<input type="password" name="pass" placeholder="enter your password" maxlength="50" required class="box">
-			
+			<input type="password" id="loginPass" name="pass" placeholder="enter your password" maxlength="50" required class="box">
+			<label>
+				<input type="checkbox" onclick="togglePasswordVisibility('loginPass')"> Show Password
+			</label>
 		</div>
-			
+
 		<p class="link">do not have an account <a href="register.php">register now</a></p>
 		<button type="submit" name="login" class="btn">login now</button>
 	</form>
@@ -66,12 +70,12 @@
 <!-- register section ends -->
 
 <!-- sweetalert cdn link -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
-	<!-- custom js link -->
-	<script type="text/javascript" src="../js/admin_script.js"></script>
+<!-- custom js link -->
+<script type="text/javascript" src="../js/admin_script.js"></script>
 
-	<?php include '../components/alert.php'; ?>
+<?php include '../components/alert.php'; ?>
 
 </body>
 </html>
